4e0f28b3930732c22e91eac13434dab96a1d6aa2
Ross Rosales <ross.erosales@gmail.com>
2022-06-10 01:45:25 -0500

    update commit

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/", async function(req, res, next) {
 
-    console.log("Connection good")
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  });
+  
+  app.listen(5000, () => {
+    console.log("Running on port 5000.");
+  });
+  
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.gunviolencearchive.org/reports/mass-shooting')
-
-    const data = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".odd td")).map(x => x.textContent)
-    })
-
-    res.send(data)
-});
+//app.get("/", async function(req, res, next) {
+//
+//    console.log("Connection good")
+//
+//    const browser = await puppeteer.launch();
+//    const page = await browser.newPage();
+//    await page.goto('https://www.gunviolencearchive.org/reports/mass-shooting')
+//
+//    const data = await page.evaluate(() => {
+//        return Array.from(document.querySelectorAll(".odd td")).map(x => x.textContent)
+//    })
+//
+//    res.send(data)
+//});
 
 app.listen(5000, () => {
     console.log('running on port 5000');
