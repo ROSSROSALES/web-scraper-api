c783b3253b55579373c1c9ebbdde2c8a601cb346
Ross Rosales <ross.erosales@gmail.com>
2022-06-12 05:16:12 -0500

    testing

--- index.js
+++ index.js
@@ -10,42 +10,42 @@ app.get('/', (req,res) => {
     res.json('Welcome to my API')
 })
 
-app.get('/data', (req,res) => {
+app.get('/data', async (req,res) => {
     axios.get('https://www.gunviolencearchive.org/reports/mass-shooting')
         .then((response) => {
             const html = response.data
             const $ = cheerio.load(html)
             const info = {}
 
-            res.json(html)
-        //    $('tr').each( (index, element) => {
-        //        const date = $(element)  
-        //            .children('td')
-        //            .eq(1)
-        //            .text() ;
-        //            info[index] = { date };
-        //        const state = $(element)
-        //            .children('td')
-        //            .eq(2)
-        //            .text();
-//
-        //        const city = $(element)
-        //            .children('td')
-        //            .eq(3)
-        //            .text();
-//
-        //        const killed = $(element)
-        //            .children('td')
-        //            .eq(5)
-        //            .text();
-        //        const injured = $(element)
-        //            .children('td')
-        //            .eq(6)
-        //            .text();
-        //        info[index] = { date, state, city, killed, injured };
-        //        data.push(info)
-        //    })
-        //    res.json(data);
+            
+            $('tr').each( (index, element) => {
+                const date = $(element)  
+                    .children('td')
+                    .eq(1)
+                    .text() ;
+                    info[index] = { date };
+                const state = $(element)
+                    .children('td')
+                    .eq(2)
+                    .text();
+
+                const city = $(element)
+                    .children('td')
+                    .eq(3)
+                    .text();
+
+                const killed = $(element)
+                    .children('td')
+                    .eq(5)
+                    .text();
+                const injured = $(element)
+                    .children('td')
+                    .eq(6)
+                    .text();
+                info[index] = { date, state, city, killed, injured };
+                data.push(info)
+            })
+            res.json(data);
         }).catch(err => console.log(err))
         
 });
