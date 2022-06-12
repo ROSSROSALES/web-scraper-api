be44814b322e0e48b100e0ccdc7263edd2d472e8
Ross Rosales <ross.erosales@gmail.com>
2022-06-12 17:43:47 -0500

    testing

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', (req,res) => {
 })
 
 app.get('/data', (req,res) => {
-    axios.get('https://www.gunviolencearchive.org/reports/mass-shooting', config)
+    axios.get('https://www.gunviolencearchive.org/reports/mass-shooting')
         .then((response) => {
             const html = response.data
             const $ = cheerio.load(html)
