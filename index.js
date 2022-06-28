0b44d5e80acd57604ebe8465084d3d0468cf381f
Ross Rosales <ross.erosales@gmail.com>
2022-06-27 19:23:25 -0500

    Testing deployment: added slice, improved search

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.get('/', (req,res) => {
 })
 
 app.get('/data', (req,res) => {
-    axios.get('https://www.gunviolencearchive.org/reports/mass-shooting')
+    axios.get('https://www.gunviolencearchive.org/reports/mass-shooting', { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36' }  })
         .then((response) => {
             const html = response.data
             const $ = cheerio.load(html)
             const info = {}
 
-            
-            $('tr').each( (index, element) => {
+            console.log($('tr').count)
+            $("table > tbody > tr").slice(0,3).each( (index, element) => {
                 const date = $(element)  
                     .children('td')
                     .eq(1)
